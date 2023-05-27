96f5e9399b0f5573d5ad7fcaf7fa89fb2a69dd45
Luther-Sparks <272786724@qq.com>
2023-05-27 20:33:53 +0800

    🐞 fix: 按照api要求修改url

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@
 // Path: frontend/src/js/config.js
 export const BASE_URL = "http://127.0.0.1:8000"
 export const API_URL = BASE_URL + "/api"
-export const LOGIN_URL = API_URL + "/login";
-export const REGISTER_URL = API_URL + "/register";
-export const LOGOUT_URL = API_URL + "/logout";
+export const LOGIN_URL = API_URL + "/user/login";
+export const REGISTER_URL = API_URL + "/user/register";
+export const LOGOUT_URL = API_URL + "/user/logout";
 export const PDF_REQ_URL = BASE_URL + "/pdf"
