7cb72b86f779ad4ea1aa20208f64a6925c33443e
Luther-Sparks <272786724@qq.com>
2023-05-28 21:34:42 +0800

    🐞 fix: 修复文献库部分请求参数错误问题

--- src/js/LibraryPage.js
+++ src/js/LibraryPage.js
@@ -1,65 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Card, CardBody, CardFooter, Button, Typography } from "@material-tailwind/react";
+import { LIBRARY_URL } from './config';
 
 const Library = () => {
-  let [libraries, setLibraries] = useState([]);
-
+  const [libraries, setLibraries] = useState([]);
+  // console.log('libraries', libraries)
   useEffect(() => {
-    axios.get('/api/library')
+    axios.get(LIBRARY_URL, {
+      params: {
+        page_num: 1,
+        page_size: 1,
+      }})
       .then(response => {
+        response = response.data;
+        console.log(response)
+        console.log('data', response.data)
         setLibraries(response.data);
+        console.log('libraries', libraries)
       })
       .catch(error => {
         console.error(error);
       });
   }, []);
 
-  libraries = [
-    {
-      "id": "string",
-      "topic": "string",
-      "desc": "string",
-      "is_public": true
-    },
-    {
-      "id": "string",
-      "topic": "string",
-      "desc": "string",
-      "is_public": true
-    },
-    {
-      "id": "string",
-      "topic": "string",
-      "desc": "string",
-      "is_public": true
-    },
-    {
-      "id": "string",
-      "topic": "string",
-      "desc": "string",
-      "is_public": true
-    },
-    {
-      "id": "string",
-      "topic": "string",
-      "desc": "string",
-      "is_public": true
-    },
-    {
-      "id": "string",
-      "topic": "string",
-      "desc": "string",
-      "is_public": true
-    },
-    {
-      "id": "string",
-      "topic": "string",
-      "desc": "string",
-      "is_public": true
-    },
-  ];
-  console.log('libraries', libraries)
+
 
   const handleDelete = (id) => {
     axios.delete(`/api/library/${id}`)
@@ -73,7 +38,7 @@ const Library = () => {
 
   return (
       <div className="grid grid-cols-4 gap-4 grow auto-cols-max auto-rows-max">
-        {libraries.map(library => (
+        { libraries > 0 ? libraries.map(library => (
           <div>
           <Card key={library.id} className="mb-4">
             <CardBody>
@@ -85,7 +50,11 @@ const Library = () => {
             </CardFooter>
           </Card>
           </div>
-        ))}
+        )) : (
+          <div className="flex center">
+            <Typography variant="h6" color="gray">暂无文献</Typography>
+          </div>
+        )}
       </div>
   );
 };
