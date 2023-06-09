75472dffc006e88606a336791afce1a3e2e9b406
Luther-Sparks <272786724@qq.com>
2023-06-09 16:37:13 +0800

    🌈 style: 按照模块功能分开config

--- src/js/config.js
+++ src/js/config.js
@@ -2,18 +2,23 @@
 // Path: frontend/src/js/config.js
 export const BASE_URL = "http://127.0.0.1:8000"
 export const API_URL = BASE_URL + "/api"
+
 export const LOGIN_URL = API_URL + "/user/login";
 export const REGISTER_URL = API_URL + "/user/register";
 export const LOGOUT_URL = API_URL + "/user/logout";
-export const PDF_REQ_URL = API_URL+ "/pdf"
+
 export const QUERY_LIBRARY_URL = API_URL + "/library"
 export const ADD_LIBRARY_URL = API_URL + "/library"
-export const QUERY_COMMENT_URL = API_URL + "/comment"
-export const QUERY_PAPER_URL = API_URL + "/paper"
-export const ADD_COMMENT_URL = API_URL + "/comment"
-export const DELETE_LIBRARY_URL = API_URL + "/library"
 export const PUT_LIBRARY_URL = API_URL + "/library"
+export const DELETE_LIBRARY_URL = API_URL + "/library"
+
+export const QUERY_COMMENT_URL = API_URL + "/comment"
+export const ADD_COMMENT_URL = API_URL + "/comment"
 export const DELETE_COMMENT_URL = API_URL + "/comment"
+
+
+export const QUERY_PAPER_URL = API_URL + "/paper"
 export const QUERY_PDF_URL = API_URL + "/paper"
 export const PUT_PDF_URL = API_URL + "/paper"
-export const USER_SETTING_URL = API_URL + "/user"
+
+export const PDF_REQ_URL = API_URL+ "/pdf"
